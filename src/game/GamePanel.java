69848916e0cf5f349196addf4a48a6225dc5cde2
GuthZa <joao9.claro99@gmail.com>
2023-08-24 19:19:44 +0100

    added images and animation

--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -2,26 +2,64 @@ package game;
 
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
+import utilz.Constants;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.util.Random;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static utilz.Constants.PlayerConstants.*;
 
 public class GamePanel extends JPanel {
 
+    private final MouseInputs mouseInputs;
+    private BufferedImage image;
+    private BufferedImage[][] animations;
+    private int animationTick, animationIndex, animationSpeed = 20;
+    private int playerAction = IDLE;
+
     private float xDelta = 100, yDelta = 100;
-    private float xDir = 0.4f, yDir = 0.4f;
-    private MouseInputs mouseInputs;
-    private Color color = new Color(150, 20, 90);
-    private Random random;
+
     public GamePanel() {
-        random = new Random();
         mouseInputs = new MouseInputs(this);
+
+        importImg();
+        loadAnimations();
+
+        setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
     }
 
+    //Images and Animations
+    private void importImg() {
+        try {
+            image = ImageIO.read(new File("resources/player_sprites.png"));
+        } catch (IOException e) {
+            System.out.println("Error loading player image at: ");
+            e.printStackTrace();
+        }
+    }
+
+    private void loadAnimations() {
+        animations = new BufferedImage[9][6];
+        for(int j = 0; j < animations.length; j++) {
+            for (int i = 0; i < animations[j].length; i++) {
+                animations[j][i] = image.getSubimage(i * 64, j * 40, 64, 40);
+            }
+        }
+    }
+
+    //Window
+    private void setPanelSize() {
+        setPreferredSize(new Dimension(1280, 800));
+    }
+
+    //Movement
     public void changeXDelta(int value) {
         this.xDelta+=value;
     }
@@ -37,29 +75,18 @@ public class GamePanel extends JPanel {
     public void paint(Graphics g) {
         super.paintComponent(g);
 
-        updateRectangle();
-        g.setColor(color);
-        g.fillRect((int) xDelta, (int) yDelta, 20, 20);
+        updateAnimationTick();
 
 
+        g.drawImage(animations[playerAction][animationIndex], (int) xDelta, (int) yDelta, 128, 80,null);
     }
 
-    private void updateRectangle() {
-        xDelta+=xDir;
-        if(xDelta > 400 || xDelta < 0) { //width
-            xDir*=-1;
-            color = getRandomColor();
+    private void updateAnimationTick() {
+        if(++animationTick >= animationSpeed) {
+            animationTick = 0;
+            if(++animationIndex >= 6) {
+                animationIndex = 0;
+            }
         }
-        yDelta+=yDir;
-        if(yDelta > 400 || yDelta < 0) { //height
-            yDir*=-1;
-        }
-    }
-
-    private Color getRandomColor() {
-        int r = random.nextInt(255);
-        int g = random.nextInt(255);
-        int b = random.nextInt(255);
-        return new Color(r,b,g);
     }
 }
--- src/game/GameWindow.java
+++ src/game/GameWindow.java
@@ -6,12 +6,11 @@ import javax.swing.*;
 
 public class GameWindow extends JFrame{
     public GameWindow(GamePanel gamePanel) {
-        setSize(400, 400);
-        setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         add(gamePanel);
         setLocationRelativeTo(null);
-
+        setResizable(false);
+        pack();
 
         setVisible(true);
     }
